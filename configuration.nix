72517f845cd1ba5839202dbcd145fff2f3fc0da9
ErwinOlie <github@erwinolie.nl>
2026-04-12 20:31:14 +0200

    Enable uBlock Origin in Firefox private browsing

--- configuration.nix
+++ configuration.nix
@@ -91,6 +91,7 @@
         "uBlock0@raymondhill.net" = {
           installation_mode = "force_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          private_browsing = true;
         };
 
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
