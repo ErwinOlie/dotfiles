808f94215add60b4aa7d51f48772d00ec9b085b7
ErwinOlie <github@erwinolie.nl>
2026-04-12 20:00:01 +0200

    Scope unstable nixpkgs via specialArgs

--- flake.nix
+++ flake.nix
@@ -10,28 +10,23 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+  outputs = { nixpkgs, nixpkgs-unstable, home-manager, ... }:
   let
     system = "x86_64-linux";
+    unstablePkgs = import nixpkgs-unstable {
+      inherit system;
+      config.allowUnfree = true;
+    };
   in {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       inherit system;
+      specialArgs = {
+        inherit unstablePkgs;
+      };
 
       modules = [
         ./configuration.nix
         home-manager.nixosModules.home-manager
-
-        # Dit vervangt jouw "unstable import"
-        ({ config, pkgs, ... }: {
-          nixpkgs.overlays = [
-            (final: prev: {
-              unstable = import nixpkgs-unstable {
-                inherit system;
-                config.allowUnfree = true;
-              };
-            })
-          ];
-        })
       ];
     };
   };
