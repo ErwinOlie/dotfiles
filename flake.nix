ac483a3b8b79190cc566d0b0c210b1b6828d54a3
Erwin Olie <github@erwinolie.nl>
2026-05-15 15:27:07 +0200

    fixes

--- flake.nix
+++ flake.nix
@@ -1,12 +1,17 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-  inputs.home-manager = {
-    url = "github:nix-community/home-manager/release-25.11";
-    inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
-
-  inputs.plasma-manager.url = "github:nix-community/plasma-manager";
 
   outputs = { nixpkgs, home-manager, plasma-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
