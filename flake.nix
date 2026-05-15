c3b2d5ab1f45bebeafcabae10bb301836e4dfa7a
Erwin Olie <github@erwinolie.nl>
2026-05-15 15:21:41 +0200

    flakes, home-manager, plasma dark mode

--- flake.nix
+++ flake.nix
@@ -1,29 +1,27 @@
 {
-  description = "Erwin's NixOS + Home Manager config";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  inputs.home-manager = {
+    url = "github:nix-community/home-manager/release-25.11";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
-    let
+  inputs.plasma-manager.url = "github:nix-community/plasma-manager";
+
+  outputs = { nixpkgs, home-manager, plasma-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-    in {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.erwin = import ./home.nix;
-          }
-        ];
-      };
+
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        plasma-manager.homeModules.plasma-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.erwin = import ./home.nix;
+        }
+      ];
     };
+  };
 }
--- home.nix
+++ home.nix
@@ -1,18 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "erwin";
-  home.homeDirectory = "/home/erwin";
   home.stateVersion = "25.11";
-
-  programs.home-manager.enable = true;
 
   programs.plasma = {
     enable = true;
-    workspace = {
-      lookAndFeel = "org.kde.breezedark.desktop";
-      theme = "breeze-dark";
-      colorScheme = "BreezeDark";
-    };
+    workspace.colorScheme = "BreezeDark";
   };
 }
